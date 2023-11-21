e863a8d9bc6105f76d00537a9f085418528aceee
ramboll.wong <ramboll.wong@hotmail.com>
2023-11-21 18:00:06 +0800

    logger examples

--- logger_example_test.go
+++ logger_example_test.go
@@ -1,21 +1,28 @@
+//go:build !binary_log
+
 package rainbowlog_test
 
 import (
-	"github.com/rambollwong/rainbowlog"
 	"os"
+
+	"github.com/rambollwong/rainbowlog"
+	"github.com/rambollwong/rainbowlog/level"
 )
 
 func ExampleNew() {
-	log := rainbowlog.New(rainbowlog.AppendsEncoderWriters(rainbowlog.JsonEnc, os.Stdout))
+	log := rainbowlog.New(
+		rainbowlog.AppendsEncoderWriters(rainbowlog.JsonEnc, os.Stdout),
+		rainbowlog.WithMetaKeys(rainbowlog.MetaLevelFieldName, rainbowlog.MsgFieldName),
+	)
 	log.Info().Msg("Hello world!").Done()
 
 	log2 := rainbowlog.New(
 		rainbowlog.AppendsEncoderWriters(rainbowlog.TextEnc, os.Stdout),
-		rainbowlog.WithMetaKeys(rainbowlog.MsgFieldName))
-	log2.Info().Msg("Hello world!").Done()
+		rainbowlog.WithMetaKeys(rainbowlog.MetaLevelFieldName, rainbowlog.MsgFieldName))
+	log2.Debug().Msg("Hello world!").Done()
 
-	// Output: {"message":"Hello world!"}
-	//Hello world!
+	// Output: {"META_LEVEL":"INFO","message":"Hello world!"}
+	//DEBUG > Hello world!
 }
 
 func ExampleLogger_SubLogger() {
@@ -29,6 +36,32 @@ func ExampleLogger_SubLogger() {
 	// Output: {"META_LABEL":"NewLabel","message":"Hello world!"}
 }
 
-//func ExampleLogger_Record() {
-//	log := rainbowlog.New(rainbowlog.AppendsEncoderWriters(rainbowlog.JsonEnc, os.Stdout))
-//}
+func ExampleLogger_Record() {
+	log := rainbowlog.New(
+		rainbowlog.AppendsEncoderWriters(rainbowlog.JsonEnc, os.Stdout),
+		rainbowlog.WithMetaKeys(rainbowlog.MetaLevelFieldName),
+	)
+	log.Record().WithLevel(level.Info).Msg("Hello world!").Done()
+	log.Record().WithLevel(level.Debug).Msg("Hello world!").Done()
+
+	// Output: {"META_LEVEL":"INFO","message":"Hello world!"}
+	//{"META_LEVEL":"DEBUG","message":"Hello world!"}
+}
+
+func ExampleLogger_Info() {
+	log := rainbowlog.New(
+		rainbowlog.AppendsEncoderWriters(rainbowlog.JsonEnc, os.Stdout),
+		rainbowlog.WithMetaKeys(rainbowlog.MetaLevelFieldName),
+	)
+	log.Info().Msg("Hello world!").Done()
+
+	// Output: {"META_LEVEL":"INFO","message":"Hello world!"}
+}
+
+// TODO: Debug
+
+// TODO: Error
+
+// TODO: CustomMetaKeyFieldName
+
+// TODO: CustomTimestamp
