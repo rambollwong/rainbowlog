e4d227b3df3061c4c75fa5c6c2232bf8568fb350
ramboll.wong <ramboll.wong@hotmail.com>
2023-11-21 17:45:07 +0800

    add global logger

---
+++ log/global_logger.go
@@ -0,0 +1,67 @@
+// Package log provides the definition and initialization operation of a global rainbow logger.
+package log
+
+import (
+	"os"
+
+	"github.com/rambollwong/rainbowlog"
+)
+
+var Logger *rainbowlog.Logger
+
+// UseDefault will initial the global Logger by using default options.
+//
+//	This is equivalent to using the following options:
+//		rainbowlog.WithLevel(level.Debug)
+//		rainbowlog.WithLabel("")
+//		rainbowlog.WithStack(false)
+//		rainbowlog.WithMetaKeys(
+//			rainbowlog.MetaTimeFieldName,
+//			rainbowlog.MetaLevelFieldName,
+//			rainbowlog.MetaLabelFieldName,
+//			rainbowlog.MetaCallerFieldName,
+//			rainbowlog.MsgFieldName,
+//		)
+//		rainbowlog.WithConsolePrint(false)
+//		rainbowlog.WithLevelFieldMarshalFunc(rainbowlog.GlobalLevelFieldMarshalFunc)
+//		rainbowlog.WithCallerMarshalFunc(rainbowlog.GlobalCallerMarshalFunc)
+//		rainbowlog.WithErrorMarshalFunc(rainbowlog.GlobalErrorMarshalFunc)
+//		rainbowlog.WithErrorStackMarshalFunc(rainbowlog.GlobalErrorStackMarshalFunc)
+//		rainbowlog.WithTimeFormat(rainbowlog.GlobalTimeFormat)
+//		rainbowlog.AppendsEncoderWriters(rainbowlog.JsonEnc, os.Stderr)
+func UseDefault() {
+	Logger = rainbowlog.New(rainbowlog.WithDefault(), rainbowlog.AppendsEncoderWriters(rainbowlog.JsonEnc, os.Stderr))
+}
+
+// UseRainbowDefault will initial the global Logger by using default options and enable rainbow console printing.
+//
+//	This is equivalent to using the following options:
+//		rainbowlog.WithLevel(level.Debug)
+//		rainbowlog.WithLabel("")
+//		rainbowlog.WithStack(false)
+//		rainbowlog.WithMetaKeys(
+//			rainbowlog.MetaTimeFieldName,
+//			rainbowlog.MetaLevelFieldName,
+//			rainbowlog.MetaLabelFieldName,
+//			rainbowlog.MetaCallerFieldName,
+//			rainbowlog.MsgFieldName,
+//		)
+//		rainbowlog.WithConsolePrint(true)
+//		rainbowlog.WithRainbowConsole(true)
+//		rainbowlog.WithLevelFieldMarshalFunc(rainbowlog.GlobalLevelFieldMarshalFunc)
+//		rainbowlog.WithCallerMarshalFunc(rainbowlog.GlobalCallerMarshalFunc)
+//		rainbowlog.WithErrorMarshalFunc(rainbowlog.GlobalErrorMarshalFunc)
+//		rainbowlog.WithErrorStackMarshalFunc(rainbowlog.GlobalErrorStackMarshalFunc)
+//		rainbowlog.WithTimeFormat(rainbowlog.GlobalTimeFormat)
+func UseRainbowDefault() {
+	Logger = rainbowlog.New(
+		rainbowlog.WithDefault(),
+		rainbowlog.WithConsolePrint(true),
+		rainbowlog.WithRainbowConsole(true),
+	)
+}
+
+// UseCustomOptions allows user to initial the global Logger by custom options.
+func UseCustomOptions(opts ...rainbowlog.Option) {
+	Logger = rainbowlog.New(opts...)
+}
